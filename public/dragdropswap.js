0596e364dccd635d4e3b7a96c11a4d7d45348d78
shubhendu <shubhendukumars09@gmail.com>
2021-06-22 20:08:36 +0530

    added input element

--- public/dragdropswap.js
+++ public/dragdropswap.js
@@ -1,3 +1,52 @@
+// This is invoked when the add todo button is clicked
+const addButton = document.getElementById('add')
+addButton.addEventListener('click', addItem)
+
+
+// This function is invoked when the add todo button is clicked
+function addItem() {
+    const input = document.getElementById('input')
+    let todoItem = input.value
+    todoList = document.getElementById('list')
+
+    // var lq = document.createElement('li')
+    var textNode1 = document.createTextNode(todoItem)
+    // li.appendChild(textNode1)
+    // document.querySelector('ul').appendChild(lq)
+
+    if (todoItem === '') {
+        var h = document.createElement('p')
+        var t = document.createTextNode('Please enter a task')
+        h.appendChild(t)
+        t.className = 'visual'
+    } else {
+        // created li
+        li = document.createElement('li')
+        // create checkbox input
+        const checkbox = document.createElement('input')
+        checkbox.type = 'checkbox'
+        checkbox.setAttribute('id', 'check')
+        // create label
+        const label = document.createElement('label')
+
+        // add these elements on page
+        todoList.appendChild(label)
+        li.appendChild(checkbox)
+        label.appendChild(textNode1)
+        li.appendChild(label)
+        todoList.insertBefore(li, todoList.childNodes[0])
+        // li.className = 'visual'
+        input.value = ''
+    }
+    setTimeout(() => {
+        li.className = 'visual'
+    }, 5)
+}
+
+
+
+
+
 function allowDrop(ev) {
     ev.preventDefault(); // default is not to allow drop
 }
